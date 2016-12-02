d568d08ebb962ea2a0f4411f501d3356eb7eb989
kayzee13 <alma.karisseq@gmail.com>
2016-12-02 13:36:36 +0800

    upload Flexbox-Grid-Layout-Pattern-1.

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// var fixmeTop = $('#test').offset().top;       // get initial position of the element
+
+// $(window).scroll(function() {                  // assign scroll event listener
+
+//     var currentScroll = $(window).scrollTop(); // get current position
+
+//     if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+//         $('#test').css({                      // scroll to that element or below it
+//             position: 'fixed',
+//             top: '0',
+//             left: '0'
+//         });
+//     } else {                                   // apply position: static
+//         $('#test').css({                      // if you scroll above it
+//             position: 'static'
+//         });
+//     }
+
+// });
